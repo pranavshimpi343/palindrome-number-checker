22e6a12c61636a275725c6ac0682ab4611f41dc9
Pranav Rajendra Shimpi <pranav.shimpi1812@gmail.com>
2026-06-06 23:38:06 +0530

    Added palindrome number checker

---
+++ Palindrome Number Checker.py
@@ -0,0 +1,15 @@
+def is_palindrome(num):
+    original = str(num)
+    reverse = original[::-1]
+
+    if original == reverse:
+        return True
+    else:
+        return False
+
+number = int(input("Enter a number: "))
+
+if is_palindrome(number):
+    print(number, "is a Palindrome Number")
+else:
+    print(number, "is not a Palindrome Number")
